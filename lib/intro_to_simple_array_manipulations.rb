42dc089904a7d6c6c83774176972a426f7474c17
Joanna E K <kalemajoanna@gmail.com>
2019-10-30 12:07:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string)
+ array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    array.push ("violet")
+    p array
+  end
+    
+  def using_unshift(array, string)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   string = "Staten Island"
+    array = array.unshift string 
+    p array
+  end 
+  
+  def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  p array.pop
+    end
+    
+    def pop_with_args(array)
+      dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+      array = array.pop(2)
+      p array
+       end 
+       
+   def using_shift(array)    
+     my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+     array = array.shift
+    p array
+   end
+      
+      def shift_with_args(array)
+        ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+        array = array.shift(2)
+        p array
+      end 
+        
+        def using_concat(array1, array2)
+           array1= ["raindrops on roses", "whiskers on kittens"]
+           array2 = ["sports cars", "flatiron school"]
+           new_array =array1.concat array2
+            p new_array
+            end
+  
